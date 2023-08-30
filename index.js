e474a619685159bcdd11af5d75b2a4e3d2413362
Sonika Kumari <niralasonika94@gmail.comm>
2023-08-30 17:42:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const multer = require("multer");
+const ejs = require("ejs");
+const path = require("path");
+//set storage engine
+const storage = multer.diskStorage({
+  destination: "./public/uploads/",
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+//init upload
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 100000000 },
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+}).single("myImage");
+
+//check file type
+
+function checkFileType(file, cb) {
+  //allowed extension
+  const filetypes = /jpeg|jpg|png|gif/;
+
+  //check ext
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+
+  //check mime
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb("Errors :Images only");
+  }
+}
+const app = express();
+
+//EJS
+
+app.set("view engine", "ejs");
+
+//public folder
+
+app.use(express.static("./public"));
+app.get("/", (req, res) => res.render("index"));
+
+app.post("/upload", (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      res.render("index", {
+        msg: err,
+      });
+    } else {
+      if (req.file == undefined) {
+        res.render("index", {
+          msg: "Error :no file selected",
+        });
+      } else {
+        res.render("index", {
+          msg: "File Uploaded",
+          file: `uploads/${req.file.filename}`,
+        });
+      }
+    }
+  });
+});
+const port = 5000;
+
+app.listen(port, console.log(`server started on ${port}`));
